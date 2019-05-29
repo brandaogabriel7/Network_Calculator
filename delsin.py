bf587d2e3300fc783aec6539498e5cc30fb3ba28
Bárbara Martins <martins.barbara04@gmail.com>
2019-05-29 19:48:18 -0300

    Corrigindo lógica e imprimindo dados
    ** impressão incompleta

--- delsin.py
+++ delsin.py
@@ -2,6 +2,7 @@ import math, io_util
 import JSONFileHandler as jfh
  
 class Delsin:
+    
     rede_obj = {
         'specs_obj': {},
         'area_trabalho': {},
@@ -12,45 +13,74 @@ class Delsin:
 
     curr_filename = None
 
+    area_trabalho = {}
+    specs_obj = {}
+    malha_horizontal = {}
+    sala_telecom = {}
+         
+
+    def perguntar_especificacoes_da_rede(self):
+
+        self.area_trabalho = self.rede_obj['area_trabalho']
+        self.specs_obj = self.rede_obj['specs_obj']
+        self.malha_horizontal = self.rede_obj['malha_horizontal']
+        self.sala_telecom = self.rede_obj['sala_telecom']
+        
+        print("Digite o número de pontos de telecom da sua rede:")
+        self.specs_obj['pts_telecom'] = int(input())
+        io_util.clear()
+        print("Digite o número de pontos de rede(simples):")
+        self.specs_obj['pts_rede'] = int(input())
+        io_util.clear()
+        print("Digite o número de pontos de CFTV IP:")
+        self.specs_obj['pts_cftv'] = int(input())
+        io_util.clear()
+        print("Digite o número de pontos de voz sobre IP:")
+        self.specs_obj['pts_voz'] = int(input())
+        io_util.clear()
+        print("Qual será o tamanho dos cabos na malha horizontal?")
+        opc = int(input())
+        io_util.clear()
+        while(int(opc) < 1):
+            print("O tamanho dos seus cabos não pode ser menor que 1, por favor digite um tamanho válido.")
+            opc = int(input())
+        self.specs_obj['tam_cabos'] = opc
+        print("O seu rack estará em um local fechado ou na área de trabalho? (Use 'a' ou 'f')")
+        if input() == 'a':
+            self.specs_obj['rack_aberto'] = True
+        else:
+            self.specs_obj['rack_aberto'] = False
+        io_util.clear()
+
 
     def calcular_materiais(self):
 
         print("Calculando...")
 
-        area_trabalho = self.rede_obj['area_trabalho']
-        specs_obj = self.rede_obj['specs_obj']
-        malha_horizontal = self.rede_obj['malha_horizontal']
-        sala_telecom = self.rede_obj['sala_telecom']
+        self.area_trabalho['espelhos_duplos'] = self.specs_obj['pts_telecom'])
+        self.area_trabalho['espelhos_simples'] = self.specs_obj['pts_rede'])
+        self.area_trabalho['tomadas'] = int(self.specs_obj['pts_rede'] + (2 * self.specs_obj['pts_telecom']))
+        self.area_trabalho['patch_cords'] = self.area_trabalho['tomadas']
+        self.area_trabalho['etiquetas'] = self.area_trabalho['tomadas'] + self.area_trabalho['espelhos_simples'] + self.area_trabalho['espelhos_duplos']
 
-        area_trabalho['espelhos'] = specs_obj['pts_telecom'] + specs_obj['pts_rede']/2 + math.ceil(specs_obj['pts_cftv']/2) + math.ceil(specs_obj['pts_voz']/2)
-        area_trabalho['tomadas'] = int(specs_obj['pts_rede'] + (2 * specs_obj['pts_telecom']) + specs_obj['pts_cftv'] + specs_obj['pts_voz'])
-        area_trabalho['patch_cords'] = area_trabalho['tomadas']
-        area_trabalho['etiquetas'] = area_trabalho['tomadas'] + area_trabalho['espelhos']
+        self.malha_horizontal['cabos_utp'] = math.ceil((self.area_trabalho['tomadas'] * self.specs_obj['tam_cabos'])/305)
+        self.malha_horizontal['etiquetas'] = 2 * self.area_trabalho['tomadas']
 
-        malha_horizontal['cabos_utp'] = math.ceil((area_trabalho['tomadas'] * specs_obj['tam_cabos'])/305)
-        malha_horizontal['etiquetas'] = 2 * area_trabalho['tomadas']
-
-        sw_redes = math.ceil(specs_obj['pts_rede'] + ((2 * specs_obj['pts_telecom'])/24))
-        sw_cftv = math.ceil(specs_obj['pts_cftv']/24)
-        sw_voz = math.ceil(specs_obj['pts_voz']/24)
-        sala_telecom['switches'] = sw_redes + sw_voz + sw_cftv
-        ppanel = {}
-        ppanel['dados'] = sw_redes
-        ppanel['voz'] = sw_voz
-        ppanel['cftv'] = sw_cftv
-        sala_telecom['patch_panels'] = ppanel
-        total_ppanel = sala_telecom['patch_panels']['dados'] + sala_telecom['patch_panels']['cftv'] + sala_telecom['patch_panels']['voz']
+        switches = math.ceil(self.specs_obj['pts_rede'] + ((2 * self.specs_obj['pts_telecom'])/24))
+        self.sala_telecom['switches'] = sw_redes + sw_voz + sw_cftv
+        self.sala_telecom['patch_panels'] = switches
         pcable = []
-        pcable.append({'cor': 'azul', 'qtd': (specs_obj['pts_rede'] + (2 * specs_obj['pts_telecom']))})
-        pcable.append({'cor': 'vermelho', 'qtd': specs_obj['pts_cftv']})
-        pcable.append({'cor': 'amarelo', 'qtd': specs_obj['pts_voz']}) 
-        sala_telecom['patch_cables'] = pcable
-        sala_telecom['etiquetas_pcable'] = 2 * area_trabalho['tomadas']
-        sala_telecom['etiquetas_ppanel'] = 24 * (total_ppanel)
-        sala_telecom['organizadores_frontais'] = total_ppanel + sala_telecom['switches']
-        tamanho_total_rack = int(sala_telecom['switches'] + total_ppanel + sala_telecom['organizadores_frontais'] + 4)
-        if(not specs_obj['rack_aberto']):
+        pcable.append({'cor': 'azul', 'qtd': (self.specs_obj['pts_rede'] + (2 * self.specs_obj['pts_telecom']) - self.specs_obj['pts_cftv'] - self.specs_obj['pts_voz'])})
+        pcable.append({'cor': 'vermelho', 'qtd': self.specs_obj['pts_cftv']})
+        pcable.append({'cor': 'amarelo', 'qtd': self.specs_obj['pts_voz']}) 
+        self.sala_telecom['patch_cables'] = pcable
+        self.sala_telecom['etiquetas_pcable'] = 2 * self.area_trabalho['tomadas']
+        self.sala_telecom['etiquetas_ppanel'] = 24 * (self.sala_telecom['patch_panels'])
+        self.sala_telecom['organizadores_frontais'] = self.sala_telecom['patch_panels'] + self.sala_telecom['switches']
+        tamanho_total_rack = int(self.sala_telecom['switches'] + self.sala_telecom['patch_panels'] + self.sala_telecom['organizadores_frontais'] + 4)
+        if(not self.specs_obj['rack_aberto']):
             tamanho_total_rack += 2
+        tamanho_total_rack = tamanho_total_rack * 3/2
         qtd_rack = 1
         if(tamanho_total_rack <=12):
             tamanho_rack = math.ceil(tamanho_total_rack/2) * 2
@@ -63,9 +93,9 @@ class Delsin:
                 tamanho_rack = tamanho_rack / qtd_rack
             tamanho_rack = tamanho_total_rack / qtd_rack
             tamanho_rack = math.ceil(tamanho_rack/4) * 4
-        sala_telecom['rack'] = {}
-        sala_telecom['rack']['tamanho'] = tamanho_rack
-        sala_telecom['rack']['qtd'] = qtd_rack
+        self.sala_telecom['rack'] = {}
+        self.sala_telecom['rack']['tamanho'] = tamanho_rack
+        self.sala_telecom['rack']['qtd'] = qtd_rack
 
         io_util.pause()
         
@@ -74,51 +104,48 @@ class Delsin:
 
     
     def listar_materiais(self):
+
         io_util.clear()
-        area_trabalho = self.rede_obj['area_trabalho']
-        specs_obj = self.rede_obj['specs_obj']
-        malha_horizontal = self.rede_obj['malha_horizontal']
-        sala_telecom = self.rede_obj['sala_telecom']
 
         print("Listando materiais...")
+
         print("Área de trabalho:")
-        print(" - Tomadas fêmea RJ-45: ", area_trabalho['tomadas'])
-        print("\tTomadas fêmea de dados (cat 6a): ", ((2*specs_obj['pts_telecom']) + specs_obj['pts_rede']))
-        print("\tTomadas fêmea de CFTV (cat 6): ", specs_obj['pts_cftv'])
-        print("\tTomadas fêmea de voz sobre IP (cat 5e): ", specs_obj['pts_voz'])
+        print(" - Tomadas fêmea RJ-45 (cat 6): ", self.area_trabalho['tomadas'])
+        print("\tTomadas fêmea de dados: ", ((2*self.specs_obj['pts_telecom']) + self.specs_obj['pts_rede']) - self.specs_obj['pts_cftv'] - self.specs_obj['pts_voz'])
+        print("\tTomadas fêmea de CFTV: ", self.specs_obj['pts_cftv'])
+        print("\tTomadas fêmea de voz sobre IP: ", self.specs_obj['pts_voz'])
+        print(" - Patch Cord (cat 6): ", self.area_trabalho['patch_cords'])
+        print(" - Espelhos simples: ", self.area_trabalho['espelhos_simples'])
+        print(" - Espelhos duplos: ", self.area_trabalho['espelhos_duplos'])
+        print(" - Etiquetas: ", self.area_trabalho['etiquetas'])
+
+        print("Malha horizontal:")
+        print(" - Cabos UTP (caixa de 305m): ", self.malha_horizontal['cabos_utp'])
+        print(" - Etiquetas: ", self.malha_horizontal['etiquetas'])
+
+        print("Sala de Telecomunicações:")
+        print(" - Patch Panels: ", self.sala_telecom['tomadas'])
+        print("\tEtiquetas: ", self.sala_telecom['pts_voz'], "U")
+        print(" - Patch Cables: ", self.sala_telecom['tomadas'])
+        print("\tEtiquetas: ", self.sala_telecom['pts_voz'], "U")
+        print(" - Organizadores frontais: ", self.sala_telecom['tomadas'])
+        #if aq
+        print(" - Organizadores laterais: ", self.sala_telecom['tomadas'])
+        print(" - Rack: ", self.sala_telecom['tomadas'])
+        print("\tTamanho: ", self.sala_telecom['pts_voz'], "U")
+        print("\tQuantidade: ", self.sala_telecom['pts_voz'])
+
+        print("Miscelânea:")
+        #if aq
+        print(" - Kit rodízio: ", self.area_trabalho['tomadas'])
+        print(" - Porta gaiola: ", self.area_trabalho['tomadas'])
+        print(" - Abraçadeiras plásticas: ", self.area_trabalho['tomadas'])
+        print(" - Abraçadeiras de velcro: ", self.area_trabalho['tomadas'])
+        print(" - Filtro de linha: ", self.area_trabalho['tomadas'])
+
         
         io_util.pause()
-        
-
-    def perguntar_especificacoes_da_rede(self):
-
-        specs_obj = self.rede_obj['specs_obj']
-        
-        print("Digite o número de pontos de telecom da sua rede:")
-        specs_obj['pts_telecom'] = int(input())
-        io_util.clear()
-        print("Digite o número de pontos de rede(simples):")
-        specs_obj['pts_rede'] = int(input())
-        io_util.clear()
-        print("Digite o número de pontos de CFTV IP:")
-        specs_obj['pts_cftv'] = int(input())
-        io_util.clear()
-        print("Digite o número de pontos de voz sobre IP:")
-        specs_obj['pts_voz'] = int(input())
-        io_util.clear()
-        print("Qual será o tamanho dos cabos na malha horizontal?")
-        opc = int(input())
-        io_util.clear()
-        while(int(opc) < 1):
-            print("O tamanho dos seus cabos não pode ser menor que 1, por favor digite um tamanho válido.")
-            opc = int(input())
-        specs_obj['tam_cabos'] = opc
-        print("O seu rack estará em um local fechado ou na área de trabalho? (Use 'a' ou 'f')")
-        if input() == 'a':
-            specs_obj['rack_aberto'] = True
-        else:
-            specs_obj['rack_aberto'] = False
-        io_util.clear()
+   
 
     def novo(self):
         print("Agora eu vou te ajudar a projetar sua rede. Para isso, preciso que você me responda algumas perguntas. Vamo começar a festa?")
